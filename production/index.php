d99f1e8cf5fbac9c3f6058ba88ea7310c443907a
HuySKM <huynamyb@gmail.com>
2018-09-23 14:58:42 +0700

    Project

--- production/index.php
+++ production/index.php
@@ -1,60 +1,13 @@
 
-<!DOCTYPE HTML>
-<html>
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-    <meta charset="utf-8">
-    <meta name="viewport" content="user-scalable=yes, width=device-width, initial-scale=1.0, maximum-scale=1">
-    <meta name="author" content="Thanh Tai">
-    <meta property="article:author" content="https://www.facebook.com/toidicode/"/>
 
-    <title>Học Lập Trình Online Miễn Phí - Toidicode.com</title>
-    <!--[if lt IE 9]>
-    <script src="https://toidicode.com/wp-content/themes/thanhtai/js/html5.js"></script>
-    <![endif]-->
-    <meta name="description"
-          content="Học lập trình Web, lập trình mobile, marketing, MMO online miễn phí"/>
-    <link rel="canonical" href="https://toidicode.com"/>
-    <link rel="publisher" href="https://plus.google.com/117776577259145153645"/>
+<div class="page-container">
+    <div class="left-content">
+        <div class="mother-grid-inner">
 
-    <meta property="og:locale" content="en_US"/>
-    <meta property="og:type" content="website"/>
-    <meta property="og:title" content="Học Lập Trình Online Miễn Phí"/>
-    <meta property="og:description"
-          content="Học lập trình Web, lập trình mobile, marketing, MMO online miễn phí"/>
-    <meta property="og:url" content="https://toidicode.com"/>
-    <meta property="og:site_name" content="Toidicode.com"/>
-    <meta property="fb:app_id" content="610336262482409"/>
-    <meta property="og:image" content="https://toidicode.com/upload/images/logo-toidicode.png"/>
-    <meta property="og:image:width" content="600"/>
-    <meta property="og:image:height" content="312"/>
-    <meta name="twitter:card" content="summary_large_image"/>
-    <meta name="twitter:description"
-          content="Học lập trình Web, lập trình mobile, marketing, MMO online miễn phí"/>
-    <meta name="twitter:title" content="Học Lập Trình Online Miễn Phí"/>
-    <meta name="twitter:image" content="https://toidicode.com/upload/images/logo-toidicode.png"/>
-    <link rel='stylesheet' id='vce_style-css' href='https://toidicode.com/wp-content/themes/thanhtai/style.min.css?ver=2.1 '
-          type='text/css' media='screen, print'/>
-    <link rel='stylesheet' id='vce_font_awesome-css'
-          href='https://toidicode.com/wp-content/themes/thanhtai/css/font-awesome.min.css' type='text/css' media='screen'/>
-    <link rel='stylesheet' id='vce_responsive-css'
-          href='https://toidicode.com/wp-content/themes/thanhtai/css/responsive.min.css?ver=1.7' type='text/css'
-          media='screen'/>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-    <link rel='shortlink' href='https://toidicode.com/'/>
-    <link rel="manifest" href="/manifest.json">
-    <link rel="shortcut icon" href="https://toidicode.com/wp-content/uploads/2016/11/favicon77.png"
-          type="image/x-icon"/>
-    <link rel="apple-touch-icon" href="https://toidicode.com/wp-content/uploads/2016/11/favicon77.png"/>
-    <meta name="theme-color" content="#3d454c">
-    <meta name="msapplication-TileColor" content="#ffffff">
-    <meta name="msapplication-TileImage" content="https://toidicode.com/wp-content/uploads/2016/11/favicon144.png"/>
-    <link href="https://toidicode.com/wp-content/themes/thanhtai/ocean.css" rel="stylesheet">
-    <!-- ads -->
-    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-</head>
+        </div>
+    </div>
 
-<body>
-<div id="vce-main">
-
-
+    <div class="clearfix"> </div>
+</div>
+<!--slide bar menu end here-->
+<!-- mother grid end here-->
